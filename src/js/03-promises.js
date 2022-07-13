9d32cd18b4c76eeab7759f13e6eb2301d6ee103a
Artur Myasnikov <102307516+Artur7707@users.noreply.github.com>
2022-07-13 09:48:37 +0300

    hw-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget;
+
+  let delayRef = Number(delay.value);
+  let stepRef = Number(step.value);
+  let amountRef = Number(amount.value);
+
+  for (let i = 1; i <= amountRef; i += 1) {
+    newPromise(i, delayRef);
+    delayRef += stepRef;
   }
+}
+
+function newPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
